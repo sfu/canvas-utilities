704382da3466ead2c86303e005abb2fa28aaad69
Steve Hillman <hillman@sfu.ca>
2013-08-20 04:35:22 -0700

    Bug fix: accept either 'x' or 'X' in the sis_id for the first character in a course number

--- process-enrollments.pl
+++ process-enrollments.pl
@@ -270,7 +270,7 @@ sub generate_enrollments
 			($term,$dept,$course,$sect) = split(/-/,$sis_id);
 
 			# Do some basic sanity checks on the sis_section_id
-			if ($term !~ /^\d+$/ || $dept !~ /^[a-zA-Z0-9]+$/ || $course !~ /^X*\d+\w?$/ || $sect !~ /^[a-zA-Z0-9]+$/) 
+			if ($term !~ /^\d+$/ || $dept !~ /^[a-zA-Z0-9]+$/ || $course !~ /^[xX]*\d+\w?$/ || $sect !~ /^[a-zA-Z0-9]+$/) 
 			{
 				push @skipmsgs,"Malformed sis_section_id \"$sis_id\" for section ".$section->{name}." in course ID $c_id. SKIPPING\n";
 				next;
