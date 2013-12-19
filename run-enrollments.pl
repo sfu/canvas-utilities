02cabaed463cbda6b6b0a86f3104b533914846ad
Steve Hillman <hillman@sfu.ca>
2013-12-19 01:03:06 -0800

    Added Andrew Leung to process-enrollments output recipients list

--- run-enrollments.pl
+++ run-enrollments.pl
@@ -17,7 +17,7 @@ $stderr = "/tmp/process-enrollments.err";
 $stdout = "/tmp/process-enrollments.out";
 $cmd = "/opt/amaint/etc/process-enrollments.pl";
 $error_email = "canvas-system\@sfu.ca";
-$info_email = "hillman\@sfu.ca,mstanger\@sfu.ca";
+$info_email = "hillman\@sfu.ca,mstanger\@sfu.ca,andrewleung\@sfu.ca";
 
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 
