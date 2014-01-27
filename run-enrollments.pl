ca1f058d4b2f789a1532911b6097be44f42323cd
Steve Hillman <hillman@sfu.ca>
2014-01-26 22:45:48 -0800

    Change email address for errors from SNOW to people

--- run-enrollments.pl
+++ run-enrollments.pl
@@ -16,7 +16,7 @@ $debug = 0;
 $stderr = "/tmp/process-enrollments.err";
 $stdout = "/tmp/process-enrollments.out";
 $cmd = "/opt/amaint/etc/process-enrollments.pl";
-$error_email = "canvas-system\@sfu.ca";
+$error_email = "hillman\@sfu.ca,mstanger\@sfu.ca,andrewleung\@sfu.ca";
 $info_email = "hillman\@sfu.ca,mstanger\@sfu.ca,andrewleung\@sfu.ca";
 
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
