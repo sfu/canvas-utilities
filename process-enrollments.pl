d6ed57c9ac45a710274bebbf97d2165f7c2dfd2c
Steve Hillman <hillman@sfu.ca>
2014-01-31 03:48:03 -0800

    blah

--- process-enrollments.pl
+++ process-enrollments.pl
@@ -213,10 +213,11 @@ sub fetch_courses_and_sections
 				    # Find the dropped section in the existing sections. We need its unique SIS_ID
 				    foreach $s (@{$course_sections})
 				    {
-					if ($s->{sis_section_id} =~ /$sec:::/)
+					my $sec_id = $s->{sis_section_id};
+					if ($sec_id =~ /$sec:::/)
 					{
-				    	    push @sections_csv,$s->{sis_section_id}.",$s_id,\"".uc($dept).uc($course)." ".uc($sec)."\",deleted";
-				    	    print "  ".$s->{sis_section_id}.",$s_id,\"".uc($dept).uc($course)." ".uc($sec)."\",deleted\n" if ($debug);
+				    	    push @sections_csv,"$sec_id,$s_id,\"".uc($dept).uc($course)." ".uc($sec)."\",deleted";
+				    	    print "  $sec_id,$s_id,\"".uc($dept).uc($course)." ".uc($sec)."\",deleted\n" if ($debug);
 					    break;
 					}
 				    }
